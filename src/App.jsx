7c09e43aa69d44bb72f98b32cb35c9e256106584
Shubham <shubhamyadav2000y@gmail.com>
2024-09-30 11:30:57 +0530

    solved the bug on importing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Components/Header';
+import Header from './components/Header';
 import Navbar from './components/Navbar';
 import MainContent from './Components/MainContent';
 import Sidebar from './components/Sidebar';
